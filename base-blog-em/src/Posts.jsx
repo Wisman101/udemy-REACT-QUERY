63a005ab2a1c08da77c293eaf0cfedfcc28b928f
wisman <lewiskimani18@gmail.com>
2024-11-04 10:13:30 +0300

    use query intro

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -6,13 +6,13 @@ import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
   const { data, isError, error, isLoading } = useQuery({
-    queryKey: ["posts"],  // key used in caching
-    queryFn: fetchPosts,
+    queryKey: ["posts", currentPage],  // key used in caching
+    queryFn: () => fetchPosts(currentPage),
     staleTime: 2000,
   });
 
@@ -38,11 +38,11 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button disabled={currentPage <= 1} onClick={() => { setCurrentPage((previousValue) => previousValue - 1)}}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button disabled={currentPage >= maxPostPage} onClick={() => { setCurrentPage((previousValue) => previousValue + 1)}}>
           Next page
         </button>
       </div>
