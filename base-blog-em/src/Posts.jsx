30b72aee7dc9570462cecbcb463099f8cb7171be
wisman <lewiskimani18@gmail.com>
2024-11-13 08:36:02 +0300

    updates

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useState, useEffect } from "react";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
@@ -8,6 +8,27 @@ const maxPostPage = 10;
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
+
+  const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationFn: (postId) => deletePost(postId),
+  })
+
+  const updateMutation = useMutation({
+    mutationFn: (postId) => updatePost(postId),
+  })
+
+  useEffect(() => {
+    if (currentPage < maxPostPage)
+    {
+        const nextPage = currentPage + 1;
+        queryClient.prefetchQuery({
+          queryKey: ["posts", nextPage],  // key used in caching
+          queryFn: () => fetchPosts(nextPage)
+        })
+    }
+  }, [currentPage, queryClient])
 
   // replace with useQuery
   const { data, isError, error, isLoading } = useQuery({
@@ -31,7 +52,11 @@ export function Posts() {
           <li
             key={post.id}
             className="post-title"
-            onClick={() => setSelectedPost(post)}
+            onClick={() => {
+              deleteMutation.reset();
+              updateMutation.reset();
+              setSelectedPost(post);
+            }}
           >
             {post.title}
           </li>
@@ -47,7 +72,11 @@ export function Posts() {
         </button>
       </div>
       <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+      {selectedPost && <PostDetail 
+        post={selectedPost} 
+        deleteMutation={deleteMutation} 
+        updateMutation={updateMutation}
+      />}
     </>
   );
 }
