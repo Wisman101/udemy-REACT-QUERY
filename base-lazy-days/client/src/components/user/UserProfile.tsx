c09b2ad959072bf9e51faf16ed9040956f4e1d4e
wisman <lewiskimani18@gmail.com>
2024-11-28 10:18:27 +0300

    update on mutation

--- base-lazy-days/client/src/components/user/UserProfile.tsx
+++ base-lazy-days/client/src/components/user/UserProfile.tsx
@@ -8,15 +8,17 @@ import {
   Input,
   Stack,
 } from "@chakra-ui/react";
+import { useMutationState } from "@tanstack/react-query";
 import { Field, Form, Formik } from "formik";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { usePatchUser } from "./hooks/usePatchUser";
+import { MUTATION_KEY, usePatchUser } from "./hooks/usePatchUser";
 import { useUser } from "./hooks/useUser";
 import { UserAppointments } from "./UserAppointments";
 
 import { useLoginData } from "@/auth/AuthContext";
+import { User } from "@shared/types";
 
 export function UserProfile() {
   const { userId } = useLoginData();
@@ -32,6 +34,15 @@ export function UserProfile() {
     }
   }, [userId, navigate]);
 
+  const pendingData = useMutationState({
+    filters: { mutationKey: [ MUTATION_KEY ], status: "pending" },
+    select: (mutation) => {
+      return mutation.state.variables as User;
+    }
+  });
+
+  const pendingUser = pendingData ? pendingData[0] : null;
+
   const formElements = ["name", "address", "phone"];
   interface FormValues {
     name: string;
@@ -44,7 +55,7 @@ export function UserProfile() {
       <Stack spacing={8} mx="auto" w="xl" py={12} px={6}>
         <UserAppointments />
         <Stack textAlign="center">
-          <Heading>Your information</Heading>
+          <Heading>information for {pendingUser ? pendingUser.name : user?.name}</Heading>
         </Stack>
         <Box rounded="lg" bg="white" boxShadow="lg" p={8}>
           <Formik
